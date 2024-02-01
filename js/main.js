6e0aec2ebed7be52cdd67380514d7978827fc76e
79234 <marusya0323@gmail.com>
2024-02-01 21:37:17 +0700

    Добавление заголовков

--- js/main.js
+++ js/main.js
@@ -1,45 +1,50 @@
 const app = new Vue({ 
-  el: '#app', 
-  data: { 
-      columns: JSON.parse(localStorage.getItem('columns')) || [ 
-          { id: 1, notes: [], maxCards: 3 }, 
-          { id: 2, notes: [], maxCards: 5 }, 
-          { id: 3, notes: [] } 
-      ], 
-      newNote: '' 
-  }, 
-  methods:{ 
-      addNote(columnId){ 
-          const column = this.columns.find(col => col.id === columnId); 
-          if(column && (!column.maxCards || column.notes.length < column.maxCards)){ 
-              column.notes.push(this.newNote); 
-              this.newNote = ''; 
-          } 
-          this.saveState();
-      },
-      moveNote(sourceColumnId, targetColumnId, noteIndex) {
-        const sourceColumn = this.columns.find(col => col.id === sourceColumnId);
-        const targetColumn = this.columns.find(col => col.id === targetColumnId);
-        if (sourceColumn && targetColumn) {
+    el: '#app', 
+    data: { 
+        columns:  JSON.parse(localStorage.getItem('columns')) || [ 
+            { id: 1, notes: [], maxCards: 3 }, 
+            { id: 2, notes: [], maxCards: 5 }, 
+            { id: 3, notes: [] } 
+        ], 
+        nnewNoteTitle: '', 
+        newNoteContent: '' 
+    }, 
+    methods:{ 
+        addNote(columnId){ 
+            const column = this.columns.find(col => col.id === columnId); 
+            if(column && (!column.maxCards || column.notes.length < column.maxCards)){ 
+                column.notes.push({ title: this.newNoteTitle, content: this.newNoteContent });
+                this.newNoteTitle = ''; 
+                this.newNoteContent = '';
+            } 
+            this.saveState();
+            
+        },
+        moveNote(sourceColumnId, targetColumnId, noteIndex) {
+          const sourceColumn = this.columns.find(col => col.id === sourceColumnId);
+          const targetColumn = this.columns.find(col => col.id === targetColumnId);
+          if (sourceColumn && targetColumn) {
             const noteToMove = sourceColumn.notes[noteIndex];
             sourceColumn.notes.splice(noteIndex, 1);
             targetColumn.notes.push(noteToMove);
-        }
-        this.checkMaxCards();
-        this.saveState();
-    },
-    checkMaxCards() {
-        const secondColumn = this.columns.find(col => col.id === 2);
-        if (secondColumn && secondColumn.notes.length === secondColumn.maxCards) {
-            const firstColumn = this.columns.find(col => col.id === 1);
-            if (firstColumn) {
-                firstColumn.maxCards = firstColumn.notes.length;
-            }
-        }
-    },
-    saveState() {
+          }
+          this.checkMaxCards();
+          this.saveState();
+          
+      },
+      checkMaxCards() {
+          const secondColumn = this.columns.find(col => col.id === 2);
+          if (secondColumn && secondColumn.notes.length === secondColumn.maxCards) {
+              const firstColumn = this.columns.find(col => col.id === 1);
+              if (firstColumn) {
+                  firstColumn.maxCards = firstColumn.notes.length;
+              }
+          }
+      },
+      saveState() {
         localStorage.setItem('columns', JSON.stringify(this.columns));
     }
 
-  } 
-});
+      
+    } 
+  });
